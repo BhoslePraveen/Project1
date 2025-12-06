ec13dd42f8ca40afd193a1a18a1ae7afa4869825
praveen <praveen.bhosle5@gmail.com>
2025-12-06 08:25:32 +0530

    First Commit

---
+++ src/com/velocity/ArrayListDemo.java
@@ -0,0 +1,17 @@
+package com.velocity;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+
+	public static void main(String[] args) {
+		ArrayList<Integer> al = new ArrayList<Integer>();
+		al.add(12);
+		al.add(15);
+		al.add(20);
+		
+		System.out.println(al);
+
+	}
+
+}
